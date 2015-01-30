1d806d7f9e01e26cb44d893400c6f8dbba3e0599
SimonFM <markhams@tcd.ie>
2015-01-30 14:15:39 +0000

    Added MatMul.c file

---
+++ matMul.c
@@ -0,0 +1,13 @@
+struct complex 
+{
+  float real;
+  float imag;
+};
+
+void matmul(struct complex ** A, struct complex ** B,
+     struct complex ** C, int a_dim1, int a_dim2, int b_dim2);
+	 
+int main()
+{
+	
+}
