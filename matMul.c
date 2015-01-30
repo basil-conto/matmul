5c5d0846bcf2945fb8d2c3551d6819553c5e9d4a
SimonFM <markhams@tcd.ie>
2015-01-30 14:49:00 +0000

    Added comments and things

--- matMul.c
+++ matMul.c
@@ -1,13 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include 
+
+/*
+  Structure that describes what a complex number is.
+  real - corresponds to the real part of a complex number.
+  imaginary - corresponds to the imaginary part of the complex number.
+*/
 struct complex 
 {
   float real;
-  float imag;
+  float imaginary;
 };
 
-void matmul(struct complex ** A, struct complex ** B,
-     struct complex ** C, int a_dim1, int a_dim2, int b_dim2);
-	 
+/*
+  A function that multiplies two matrices and stores the result in matrix C
+  The dimensions are the size of the matrices.
+*/
+void matmul(struct complex ** A, struct complex ** B, struct complex ** C, 
+            int a_dim1, int a_dim2, int b_dim2)
+{
+    
+}
+
+/*
+  The main function that calls and tests the program.
+*/
 int main()
 {
-	
+  
 }
