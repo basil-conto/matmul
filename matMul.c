417bec818aa5b28630bda0226e3476a1141e7a27
Basil L. Contovounesios <contovob@tcd.ie>
2015-02-03 19:05:05 +0000

    Time both functions, slight code refactoring

--- matMul.c
+++ matMul.c
@@ -1,6 +1,6 @@
-/* 
+/*
   Test and timing harness program for developing a dense matrix
-  multiplication routine for the CS3014 module 
+  multiplication routine for the CS3014 module
 */
 
 #include <stdio.h>
@@ -11,15 +11,15 @@
 
 #include <xmmintrin.h>
 
-/* 
+/*
   The following two definitions of DEBUGGING control whether or not
   debugging information is written out. To put the program into
   debugging mode, uncomment the following line: 
 */
+
+// #define DEBUGGING(_x) _x 
+
 /*
-  #define DEBUGGING(_x) _x 
-*/
-/* 
   To stop the printing of debugging information, use the following line: 
 */
 #define DEBUGGING(_x)
@@ -30,21 +30,20 @@ struct complex
   float imag;
 };
 
-/* write matrix to stdout */
+/*
+  Write matrix to stdout
+*/
 void write_out(struct complex ** a, int dim1, int dim2)
 {
-  int i, j;
-
-  for ( i = 0; i < dim1; i++ )
+  for ( int i = 0; i < dim1; i++ )
   {
-    for ( j = 0; j < dim2 - 1; j++ )
+    for ( int j = 0; j < dim2 - 1; j++ )
     {
       printf("%f + %fi ", a[i][j].real, a[i][j].imag);
     }
-    printf("%f +%fi\n", a[i][dim2-1].real, a[i][dim2-1].imag);
+    printf("%f + %fi\n", a[i][dim2-1].real, a[i][dim2-1].imag);
   }
 }
-
 
 /* 
   Create new empty matrix 
@@ -53,11 +52,10 @@ struct complex ** new_empty_matrix(int dim1, int dim2)
 {
   struct complex ** result = malloc(sizeof(struct complex*) * dim1);
   struct complex * new_matrix = malloc(sizeof(struct complex) * dim1 * dim2);
-  int i;
 
-  for ( i = 0; i < dim1; i++ ) 
+  for ( int i = 0; i < dim1; i++ ) 
   {
-    result[i] = &(new_matrix[i*dim2]);
+    result[i] = &new_matrix[i * dim2];
   }
 
   return result;
@@ -68,12 +66,11 @@ struct complex ** new_empty_matrix(int dim1, int dim2)
 */
 struct complex ** copy_matrix(struct complex ** source_matrix, int dim1, int dim2)
 {
-  int i, j;
   struct complex ** result = new_empty_matrix(dim1, dim2);
 
-  for ( i = 0; i < dim1; i++ ) 
+  for ( int i = 0; i < dim1; i++ ) 
   {
-    for ( j = 0; j < dim2; j++ )
+    for ( int j = 0; j < dim2; j++ )
     {
       result[i][j] = source_matrix[i][j];
     }
@@ -87,9 +84,9 @@ struct complex ** copy_matrix(struct complex ** source_matrix, int dim1, int dim
 struct complex ** gen_random_matrix(int dim1, int dim2)
 {
   struct complex ** result;
-  int i, j;
   struct timeval seedtime;
   int seed;
+  long long upper, lower;
 
   result = new_empty_matrix(dim1, dim2);
 
@@ -103,12 +100,12 @@ struct complex ** gen_random_matrix(int dim1, int dim2)
   /* 
     Fill the matrix with random numbers 
   */
-  for ( i = 0; i < dim1; i++ )
+  for ( int i = 0; i < dim1; i++ )
   {
-    for ( j = 0; j < dim2; j++ )
+    for ( int j = 0; j < dim2; j++ )
     {
-      long long upper = random();
-      long long lower = random();
+      upper = random();
+      lower = random();
       result[i][j].real = (float)((upper << 32) | lower);
       upper = random();
       lower = random();
@@ -124,26 +121,25 @@ struct complex ** gen_random_matrix(int dim1, int dim2)
 */
 void check_result(struct complex ** result, struct complex ** control, int dim1, int dim2)
 {
-  int i, j;
+  double diff = 0.0;
   double sum_abs_diff = 0.0;
   const double EPSILON = 0.0625;
 
-  for ( i = 0; i < dim1; i++ ) 
+  for ( int i = 0; i < dim1; i++ ) 
   {
-    for ( j = 0; j < dim2; j++ )
+    for ( int j = 0; j < dim2; j++ )
     {
-      double diff;
       diff = abs(control[i][j].real - result[i][j].real);
-      sum_abs_diff = sum_abs_diff + diff;
+      sum_abs_diff += diff;
       diff = abs(control[i][j].imag - result[i][j].imag);
-      sum_abs_diff = sum_abs_diff + diff;
+      sum_abs_diff += diff;
     }
   }
 
   if ( sum_abs_diff > EPSILON ) 
   {
     fprintf(stderr, "WARNING: sum of absolute differences (%f) > EPSILON (%f)\n",
-    sum_abs_diff, EPSILON);
+            sum_abs_diff, EPSILON);
   }
 }
 
@@ -153,23 +149,19 @@ void check_result(struct complex ** result, struct complex ** control, int dim1,
 void matmul(struct complex ** A, struct complex ** B, struct complex ** C, 
             int a_dim1, int a_dim2, int b_dim2)
 {
-  int i, j, k;
+  struct complex sum, product;
 
-  for ( i = 0; i < a_dim1; i++ ) 
+  for ( int i = 0; i < a_dim1; i++ ) 
   {
-    for( j = 0; j < b_dim2; j++ ) 
+    for( int j = 0; j < b_dim2; j++ ) 
     {
-      struct complex sum;
       sum.real = 0.0;
       sum.imag = 0.0;
-      for ( k = 0; k < a_dim2; k++ ) 
+      for ( int k = 0; k < a_dim2; k++ ) 
       {
         // the following code does: sum += A[i][k] * B[k][j];
-        struct complex product;
-        product.real = A[i][k].real * B[k][j].real - A[i][k].imag * B[k][j].imag;
-        product.imag = A[i][k].real * B[k][j].imag + A[i][k].imag * B[k][j].real;
-        sum.real += product.real;
-        sum.imag += product.imag;
+        sum.real += A[i][k].real * B[k][j].real - A[i][k].imag * B[k][j].imag;
+        sum.imag += A[i][k].real * B[k][j].imag + A[i][k].imag * B[k][j].real;
       }
       C[i][j] = sum;
     }
@@ -200,7 +192,7 @@ int main(int argc, char ** argv)
 
   if ( argc != 5 ) 
   {
-    fprintf(stderr, "Usage: matmul-harness <A nrows> <A ncols> <B nrows> <B ncols>\n");
+    fprintf(stderr, "Usage: matMul <A nrows> <A ncols> <B nrows> <B ncols>\n");
     exit(1);
   }
   else 
@@ -211,18 +203,18 @@ int main(int argc, char ** argv)
     b_dim2 = atoi(argv[4]);
   }
 
-  /* 
+  /*
     Check the matrix sizes are compatible
   */
   if ( a_dim2 != b_dim1 )
   {
     fprintf(stderr,
-    "FATAL number of columns of A (%d) does not match number of rows of B (%d)\n",
-    a_dim2, b_dim1);
+            "FATAL number of columns of A (%d) does not match number of rows of B (%d)\n",
+            a_dim2, b_dim1);
     exit(1);
   }
 
-  /* 
+  /*
     Allocate the matrices
   */
   A = gen_random_matrix(a_dim1, a_dim2);
@@ -232,28 +224,41 @@ int main(int argc, char ** argv)
 
   DEBUGGING(write_out(A, a_dim1, a_dim2));
 
-  /* 
+  /*
+    Record starting time of naive matrix multiplication
+  */
+  gettimeofday(&start_time, NULL);
+
+  /*
     Use a simple matmul routine to produce control result 
   */
   matmul(A, B, control_matrix, a_dim1, a_dim2, b_dim2);
 
-  /* 
+  /*
+    Record finishing time of naive matrix multiplication
+  */
+  gettimeofday(&stop_time, NULL);
+  mul_time = ((stop_time.tv_sec - start_time.tv_sec) * 1000000L) +
+             (stop_time.tv_usec - start_time.tv_usec);
+  printf("matmul time:      %lld microseconds\n", mul_time);
+
+  /*
     Record starting time 
   */
   gettimeofday(&start_time, NULL);
 
-  /* 
+  /*
     Perform matrix multiplication
   */
   team_matmul(A, B, C, a_dim1, a_dim2, b_dim2);
 
-  /* 
+  /*
     Record finishing time 
   */
   gettimeofday(&stop_time, NULL);
-  mul_time = (stop_time.tv_sec - start_time.tv_sec) * 1000000L +
-  (stop_time.tv_usec - start_time.tv_usec);
-  printf("Matmul time: %lld microseconds\n", mul_time);
+  mul_time = ((stop_time.tv_sec - start_time.tv_sec) * 1000000L) +
+             (stop_time.tv_usec - start_time.tv_usec);
+  printf("team_matmul time: %lld microseconds\n", mul_time);
 
   DEBUGGING(write_out(C, a_dim1, b_dim2));
 
